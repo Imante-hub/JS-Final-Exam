f023821ad07868d22d9ccc9ba4f96b295a119ffd
Imante.Gimbutiene <imante.gimb@gmail.com>
2022-05-17 19:22:25 +0300

    second day commit

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,24 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  getIntroduction = function () {
+    return `${this.title}, ${this.director}, ${this.budget}`;
+  };
+  getBudget() {
+    return this.budget - this.budget;
+  }
+}
+const Movie1 = new Movie("Avatar", "James Cameron", "237 000 000");
+const Movie2 = new Movie("Titanic", "James Cameron", "100 000 001");
+
+console.log(`${Movie1.getIntroduction()}, biudžetas ${Movie1.getBudget()} USD`);
+
+console.log(`${Movie2.getIntroduction()}, biudžetas ${Movie2.getBudget()} USD`);
